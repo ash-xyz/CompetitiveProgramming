d39b76974f247100fa9b063e860b93d557854b9b
Ashraf Ali <ashraf.ali@ucdconnect.ie>
2020-05-12 16:30:51 +0100

    Quick Fix

--- Templates/template.java
+++ Templates/template.java
@@ -1,9 +1,11 @@
+package Templates;
+
 import java.io.*;
 import java.util.*;
 
-public class main {
+public class template {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(new File("dicegame"));
+        Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
             
         }
