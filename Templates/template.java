658ba90948b5e4d56d948e91a974d04a4c103ee7
Ashraf Ali <ashraf.ali@ucdconnect.ie>
2020-05-12 16:19:02 +0100

    Templates for Java,C++ & Python

---
+++ Templates/template.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(new File("dicegame"));
+        while (sc.hasNext()) {
+            
+        }
+    }
+}
