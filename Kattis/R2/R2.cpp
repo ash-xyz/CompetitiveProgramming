b1f52c8055c2eb4d0be34bfd36a1c24beb6ef3a3
Ashraf Ali <ashraf.ali@ucdconnect.ie>
2020-05-14 02:36:55 +0100

    Solutions to R2

---
+++ Kattis/R2/R2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int r, s;
+    cin >> r >> s;
+    cout << 2 * s - r;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+}
