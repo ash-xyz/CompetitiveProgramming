5d70d05bc7f683c041808e9f6902c0054c85f1d9
Ashraf Ali <ashraf.ali@ucdconnect.ie>
2020-05-15 13:56:17 +0100

    Solutions to Solving For Carrots

---
+++ Kattis/Solving For Carrots/carrots.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, C;
+    string dp[N];
+    cin >> N >> C;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> dp[i];
+    }
+    cout << C;
+}
