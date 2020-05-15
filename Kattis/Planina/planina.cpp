aebd23ad48466a98ab1fc2491502ba67efc6d568
Ashraf Ali <ashraf.ali@ucdconnect.ie>
2020-05-15 14:43:18 +0100

    Solutions to planina

---
+++ Kattis/Planina/planina.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    unsigned long long n;
+    cin >> n;
+    cout << pow((pow(2, n) + 1), 2);
+}
