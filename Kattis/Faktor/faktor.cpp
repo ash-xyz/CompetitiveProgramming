9dc8c7c7fdc45f35f630603f50951b29d930c471
Ashraf Ali <ashraf.ali@ucdconnect.ie>
2020-05-16 12:01:35 +0100

    Solutions to Faktor

---
+++ Kattis/Faktor/faktor.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a * (b - 1) + 1 << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+}
