1dfc77c71cb01e4ba86e29b26d0b58a5bc449e2c
Ashraf Ali <ashraf.ali@ucdconnect.ie>
2020-05-20 03:03:21 +0100

    Solutions to A Different Problem

---
+++ Kattis/A Different Problem/differentProblem.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long A, B;
+    while (cin >> A >> B)
+    {
+        cout << abs(A - B) << endl;
+    }
+}
