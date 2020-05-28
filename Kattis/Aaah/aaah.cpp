afa4ed4fd9fb2da5b765a3caee8663b426cd3e11
Ashraf Ali <ashrafali379@gmail.com>
2020-05-28 19:06:15 +0100

    Solutions to Aaah

---
+++ Kattis/Aaah/aaah.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    string s;
+    string s1;
+
+    cin >> s >> s1;
+
+    s.size() >= s1.size() ? cout << "go" : cout << "no";
+    cout << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+}
