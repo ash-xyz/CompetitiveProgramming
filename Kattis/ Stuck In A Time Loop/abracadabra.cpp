985b2de5ba41414bc9ee0f3711b1fa89051e6ce1
Ashraf Ali <ashrafali379@gmail.com>
2020-05-30 20:12:00 +0100

    Solutions to Stuck in Time

---
+++ Kattis/ Stuck In A Time Loop/abracadabra.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cout << i + 1 << " Abracadabra" << endl;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+}
