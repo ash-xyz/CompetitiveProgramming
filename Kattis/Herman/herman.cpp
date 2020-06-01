7fd3029378b4c96e1fa09bd6fceb3d487847c121
Ashraf Ali <ashrafali379@gmail.com>
2020-06-01 11:10:53 +0100

    Solution to Herman

---
+++ Kattis/Herman/herman.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int R;
+    cin >> R;
+    cout << fixed;
+    cout.precision(6);
+    cout << (double)M_PI * R * R << endl;
+    cout << (double)2 * R * R << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+}
